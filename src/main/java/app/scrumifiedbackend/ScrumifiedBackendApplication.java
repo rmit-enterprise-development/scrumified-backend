396e3b445ac556bb12199c525bca4767c2bcd9c9
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-03-30 11:59:56 +0700

    Initial commit

---
+++ src/main/java/app/scrumifiedbackend/ScrumifiedBackendApplication.java
@@ -0,0 +1,13 @@
+package app.scrumifiedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumifiedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumifiedBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/scrumifiedbackend/ScrumifiedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.scrumifiedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumifiedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
