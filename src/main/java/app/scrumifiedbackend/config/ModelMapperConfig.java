d1fea504bc1f8324a0cc26a1d55cf5f02ae6e598
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-01 13:22:57 +0700

    Thach 4: update the get project

--- src/main/java/app/scrumifiedbackend/config/ModelMapperConfig.java
+++ src/main/java/app/scrumifiedbackend/config/ModelMapperConfig.java
@@ -1,6 +1,12 @@
 package app.scrumifiedbackend.config;
 
+import app.scrumifiedbackend.dto.ProjectDto;
+import app.scrumifiedbackend.dto.UserDto;
+import app.scrumifiedbackend.entity.Project;
+import app.scrumifiedbackend.entity.User;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.TypeMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +19,18 @@ public class ModelMapperConfig {
         modelMapper.getConfiguration()
                 .setMatchingStrategy(MatchingStrategies.STRICT)
                 .setSkipNullEnabled(true);
+
+        modelMapper.addMappings(configMapProjectToProjectDto());
         return modelMapper;
     }
-}
+
+    PropertyMap<Project, ProjectDto> configMapProjectToProjectDto() {
+        return new PropertyMap<>() {
+            @Override
+            protected void configure() {
+                map().setOwnerId(source.getOwner().getId());
+                map().setParticipantsId(source.getParticipatedId());
+            }
+        };
+    }
+    }
