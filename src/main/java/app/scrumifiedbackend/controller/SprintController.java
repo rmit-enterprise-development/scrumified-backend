45204e17c876be25e65af289ffc269b27d631c01
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-04-06 17:14:25 +0700

    update sprint

--- src/main/java/app/scrumifiedbackend/controller/SprintController.java
+++ src/main/java/app/scrumifiedbackend/controller/SprintController.java
@@ -1,4 +1,33 @@
 package app.scrumifiedbackend.controller;
 
+import app.scrumifiedbackend.assembler.SprintDtoEntityAssembler;
+import app.scrumifiedbackend.dto.SprintDto;
+import app.scrumifiedbackend.service.interface_service.SprintService;
+import lombok.AllArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+@CrossOrigin
+@AllArgsConstructor
 public class SprintController {
+    private SprintService sprintService;
+    private SprintDtoEntityAssembler sprintDtoEntityAssembler;
+
+    @GetMapping("/sprints/{sprintId}")
+    public EntityModel<SprintDto> getSprint(@PathVariable("sprintId") Long id) {
+        return sprintDtoEntityAssembler.toModel(sprintService.findOne(id));
+    }
+
+    @PutMapping("/sprints/{sprintId}")
+    public EntityModel<SprintDto> updateSprint(@PathVariable("sprintId") Long id, @RequestBody SprintDto sprintDto) {
+        SprintDto updatedSprint = sprintService.update(id, sprintDto);
+        return sprintDtoEntityAssembler.toModel(updatedSprint);
+    }
+
+    @DeleteMapping("/sprints/{sprintId}")
+    public void deleteSprint(@PathVariable("sprintId") Long id) {
+        sprintService.delete(id);
+    }
 }
--- src/main/java/app/scrumifiedbackend/dto/SprintDto.java
+++ src/main/java/app/scrumifiedbackend/dto/SprintDto.java
@@ -1,4 +1,18 @@
 package app.scrumifiedbackend.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class SprintDto {
+    private Long id;
+    private String goal;
+    private String status;
+    private String defOfDone;
+    private String startDate;
+    private String endDate;
+    private Long projectId;
 }
